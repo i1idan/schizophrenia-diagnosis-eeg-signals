0ad019eaf595e08b1d3415bac4962b7e9924fe12
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-01-23 09:39:10 +0330

    [Transformer] Fix new loading data

--- data/load_data.py
+++ data/load_data.py
@@ -2,14 +2,20 @@ import numpy as np
 from typing import Tuple
 from pyts.image import GramianAngularField
 import scipy.io
-import cv2
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-def load_data(data_path: str = './DATA.mat', seed: int = 1234,
-              as_signals=False) -> Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+def load_data(model_name, **kwargs):
+    return {
+        "Transformer": load_data_1d,
+        "FFTCustom": load_data_2d
+    }[model_name](**kwargs)
 
+
+def load_data_1d(data_path: str = './DATA.mat', seed: int = 1234) -> Tuple[
+    Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+    print('[INFO] Loading 1d data')
     # Load data
     data = scipy.io.loadmat(data_path)
     # extract-information
@@ -28,111 +34,132 @@ def load_data(data_path: str = './DATA.mat', seed: int = 1234,
     channel_size = CHAN.shape[1]  # number of channels
     sub = 15000  # We use a small subset of information
 
+    # create labels
+    n = np.zeros(shape=(healthy_samples, 1))
+    s = np.ones(shape=(schizo_samples, 1))
+    labels = np.concatenate((s, n), axis=0)
+    data = np.array(np.concatenate((H, S), axis=0))
+    new_data = []
 
-    if as_signals:
-      # create labels
-      n = np.zeros(shape=(healthy_samples, 1))
-      s = np.ones(shape=(schizo_samples, 1))
-      labels = np.concatenate((s, n), axis=0)
-      data = np.array(np.concatenate((H, S), axis=0))
-      new_data = []
-
-      for data in data:
+    for data in data:
         new_data.append(data)
 
-      data = np.array(new_data)
-      del new_data
-      data = data[:, 0:19, 0:sub]
+    data = np.array(new_data)
+    del new_data
+    data = data[:, 0:19, 0:sub]
 
-      # spread channels
-      data_new = []
-      labels_new = []
+    # spread channels
+    data_new = []
+    labels_new = []
 
-      for i, _ in enumerate(data):
+    for i, _ in enumerate(data):
         for j, _ in enumerate(data[i]):
-          data_new.append(data[i][j])
-          labels_new.append(labels[i])
+            data_new.append(data[i][j])
+            labels_new.append(labels[i])
 
-      data = np.array(data_new)
-      labels = np.array(labels_new)
-      print(data.shape)
-      print(labels.shape)
-      del data_new, labels_new
-      x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2,
-                                                    shuffle=True, random_state=seed,
-                                                    stratify=labels)
+    data = np.array(data_new)
+    labels = np.array(labels_new)
+    print(data.shape)
+    print(labels.shape)
+    del data_new, labels_new
 
-      sc = StandardScaler()
-      x_train = sc.fit_transform(x_train)
-      x_test = sc.transform(x_test)
-      x_train = x_train.reshape(-1, 500, 30)
-      x_test = x_test.reshape(-1, 500, 30)
-      print(f"[INFO] x_train.shape: {x_train.shape}, y_train.shape: {y_train.shape}")
-      print(f"[INFO] x_test.shape: {x_test.shape}, y_test.shape: {y_test.shape}")
-      return (x_train, y_train), (x_test, y_test)
-
-    else:
-        
-      print(
-          f"[INFO] healthy_samples: {healthy_samples}, schizo_samples: {schizo_samples}, channel_size: {channel_size}, and sub: {sub}")
-      # Get normal samples
-      normal = []
-
-      for j in range(healthy_samples):
-          for i in range(channel_size):
-              x = H[j][i, :sub]
-              x = x.reshape(1, -1)
-              gasf = GramianAngularField(image_size=112, method='summation')
-              x_gasf = gasf.fit_transform(x)
-              normal.append(x_gasf[0])
+    x_train, x_test, y_train, y_test = train_test_split(data,
+                                                        labels,
+                                                        test_size=0.2,
+                                                        shuffle=True,
+                                                        random_state=seed,
+                                                        stratify=labels)
+    print("[INFO] Scaling the inputs")
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+    x_train = x_train.reshape(-1, 500, 30)
+    x_test = x_test.reshape(-1, 500, 30)
+    print(f"[INFO] x_train.shape: {x_train.shape}, y_train.shape: {y_train.shape}")
+    print(f"[INFO] x_test.shape: {x_test.shape}, y_test.shape: {y_test.shape}")
+    return (x_train, y_train), (x_test, y_test)
 
 
-      for j in range(healthy_samples):
-          for i in range(channel_size):
-              x = H[j][i, :sub]
-              x = x.reshape(1, -1)
-              gadf = GramianAngularField(image_size=112, method='difference')
-              x_gadf = gadf.fit_transform(x)
-              normal.append(x_gadf[0])
-      normal = np.array(normal)
-      print(f"[INFO] normal samples: {normal.shape}")
+def load_data_2d(data_path: str = './DATA.mat', seed: int = 1234) -> Tuple[
+    Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+    print('[INFO] Loading 2d data')
+    # Load data
+    data = scipy.io.loadmat(data_path)
+    # extract-information
+    H = data['H'].reshape(-1)  # healthy
+    print(f"[INFO] H.shape: {H.shape}, each sample has (channels, values): {H[0].shape} !")
+    S = data['S'].reshape(14)  # schizopherni
+    print(f"[INFO] S.shape: {H.shape}, each sample has (channels, values): {S[0].shape} !")
+    print(f"[INFO] S.shape: {S.shape}")
+    CHAN = data['CHAN']  # channels
+    print(f"[INFO] CHAN.shape: {CHAN.shape}")
+    Fs = data['Fs']  # ferequency
+    print(f"[INFO] Fs.shape: {Fs.shape}")
+    # Define Static variable
+    healthy_samples = H.shape[0]  # number of healthy samples
+    schizo_samples = S.shape[0]  # number of confirmed schizophrenia samples
+    channel_size = CHAN.shape[1]  # number of channels
+    sub = 15000  # We use a small subset of information
 
-      # Get schizophrenia samples
-      schizo = []
+    print(
+        f"[INFO] healthy_samples: {healthy_samples}, schizo_samples: {schizo_samples}, channel_size: {channel_size}, and sub: {sub}")
+    # Get normal samples
+    normal = []
 
-      for j in range(schizo_samples):
-          for i in range(channel_size):
-              x = S[j][i, :sub]
-              x = x.reshape(1, -1)
-              gasf = GramianAngularField(image_size=112, method='summation')
-              x_gasf = gasf.fit_transform(x)
-              schizo.append(x_gasf[0])
+    for j in range(healthy_samples):
+        for i in range(channel_size):
+            x = H[j][i, :sub]
+            x = x.reshape(1, -1)
+            gasf = GramianAngularField(image_size=112, method='summation')
+            x_gasf = gasf.fit_transform(x)
+            normal.append(x_gasf[0])
 
-      for j in range(schizo_samples):
-          for i in range(channel_size):
-              x = S[j][i, :sub]
-              x = x.reshape(1, -1)
-              gadf = GramianAngularField(image_size=112, method='difference')
-              x_gadf = gadf.fit_transform(x)
-              schizo.append(x_gadf[0])
-      schizo = np.array(schizo)
-      print(f"[INFO] schizo samples: {schizo.shape}")
-      # get data and labels
-      labels = np.concatenate([np.zeros(len(normal)), np.ones(len(schizo))])
-      data = np.concatenate([normal, schizo], axis=0)
-      data = np.array([np.expand_dims(d, axis=-1) for d in data])
-      print(f"[INFO] labels: {labels.shape}")
-      print(f"[INFO] data: {data.shape}")
+    for j in range(healthy_samples):
+        for i in range(channel_size):
+            x = H[j][i, :sub]
+            x = x.reshape(1, -1)
+            gadf = GramianAngularField(image_size=112, method='difference')
+            x_gadf = gadf.fit_transform(x)
+            normal.append(x_gadf[0])
+    normal = np.array(normal)
+    print(f"[INFO] normal samples: {normal.shape}")
 
-      # Split the dataset
-      x_train, x_test, y_train, y_test = train_test_split(data,
-                                                          labels,
-                                                          test_size=0.2,
-                                                          random_state=seed,
-                                                          shuffle=True,
-                                                          stratify=labels
-                                                          )
+    # Get schizophrenia samples
+    schizo = []
 
-      print(f"[INFO] x_train.shape: {x_train.shape}, y_train.shape: {y_train.shape}")
-      print(f"[INFO] x_test.shape: {x_test.shape}, y_test.shape: {y_test.shape}")
-      return (x_train, y_train), (x_test, y_test)
+    for j in range(schizo_samples):
+        for i in range(channel_size):
+            x = S[j][i, :sub]
+            x = x.reshape(1, -1)
+            gasf = GramianAngularField(image_size=112, method='summation')
+            x_gasf = gasf.fit_transform(x)
+            schizo.append(x_gasf[0])
+
+    for j in range(schizo_samples):
+        for i in range(channel_size):
+            x = S[j][i, :sub]
+            x = x.reshape(1, -1)
+            gadf = GramianAngularField(image_size=112, method='difference')
+            x_gadf = gadf.fit_transform(x)
+            schizo.append(x_gadf[0])
+    schizo = np.array(schizo)
+    print(f"[INFO] schizo samples: {schizo.shape}")
+    # get data and labels
+    labels = np.concatenate([np.zeros(len(normal)), np.ones(len(schizo))])
+    data = np.concatenate([normal, schizo], axis=0)
+    data = np.array([np.expand_dims(d, axis=-1) for d in data])
+    print(f"[INFO] labels: {labels.shape}")
+    print(f"[INFO] data: {data.shape}")
+
+    # Split the dataset
+    x_train, x_test, y_train, y_test = train_test_split(data,
+                                                        labels,
+                                                        test_size=0.2,
+                                                        random_state=seed,
+                                                        shuffle=True,
+                                                        stratify=labels
+                                                        )
+
+    print(f"[INFO] x_train.shape: {x_train.shape}, y_train.shape: {y_train.shape}")
+    print(f"[INFO] x_test.shape: {x_test.shape}, y_test.shape: {y_test.shape}")
+    return (x_train, y_train), (x_test, y_test)
--- models/transformer.py
+++ models/transformer.py
@@ -7,7 +7,7 @@ from tensorflow.keras.layers import TimeDistributed
 from tensorflow.keras.layers import Input, Dense, Dropout, GlobalAveragePooling1D
 from tensorflow.keras.layers import Flatten, Reshape, LSTM, Average
 
- 
+
 class MultiHeadSelfAttention(layers.Layer):
     def __init__(self, embed_dim, num_heads=8):
         super(MultiHeadSelfAttention, self).__init__()
@@ -22,7 +22,7 @@ class MultiHeadSelfAttention(layers.Layer):
         self.key_dense = layers.Dense(embed_dim)
         self.value_dense = layers.Dense(embed_dim)
         self.combine_heads = layers.Dense(embed_dim)
- 
+
     def attention(self, query, key, value):
         score = tf.matmul(query, key, transpose_b=True)
         dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
@@ -30,11 +30,11 @@ class MultiHeadSelfAttention(layers.Layer):
         weights = tf.nn.softmax(scaled_score, axis=-1)
         output = tf.matmul(weights, value)
         return output, weights
- 
+
     def separate_heads(self, x, batch_size):
         x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
         return tf.transpose(x, perm=[0, 2, 1, 3])
- 
+
     def call(self, inputs):
         # x.shape = [batch_size, seq_len, embedding_dim]
         batch_size = tf.shape(inputs)[0]
@@ -61,20 +61,20 @@ class MultiHeadSelfAttention(layers.Layer):
             concat_attention
         )  # (batch_size, seq_len, embed_dim)
         return output
- 
- 
+
+
 class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
         super(TransformerBlock, self).__init__()
         self.att = MultiHeadSelfAttention(embed_dim, num_heads)
         self.ffn = keras.Sequential(
-            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim), ]
         )
         self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
         self.dropout1 = layers.Dropout(rate)
         self.dropout2 = layers.Dropout(rate)
- 
+
     def call(self, inputs, training):
         attn_output = self.att(inputs)
         attn_output = self.dropout1(attn_output, training=training)
@@ -82,8 +82,8 @@ class TransformerBlock(layers.Layer):
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
         return self.layernorm2(out1 + ffn_output)
- 
- 
+
+
 class PositionEmbedding(layers.Layer):
     def __init__(self, maxlen, vocab_size, embed_dim):
         super(PositionEmbedding, self).__init__()
@@ -94,51 +94,48 @@ class PositionEmbedding(layers.Layer):
         positions = tf.range(start=0, limit=500, delta=1)
         positions = self.pos_emb(positions)
         return x + positions
- 
 
 
-class Transformer(object):
-  def __init__(self, **kwargs):
-    pass
+class Transformer:
 
-  @staticmethod
-  def TimeSeriesTransformer(input_length,
-                            embed_dim=30, num_heads=2, ff_dim=2,
-                            classes=1, dense_units=20):
-    """
-    TimeSeries Transformer Classifier Instance.
-    :param embed_dim: Embedding size for each token.
-    :param num_heads: Number of attention heads.
-    :param ff_dim: Hidden layer size in feed forward network inside transformer.
-    """
-  
-    inputs = layers.Input(shape=(500, 30))
-    embedding_layer = PositionEmbedding(vocab_size=12158,
-                                        embed_dim=30,
-                                        maxlen=500)
-    x = embedding_layer(inputs)
-    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim,)
-    x = transformer_block(x)
-    x = layers.Flatten()(x)
-    x = layers.Dense(dense_units, activation="relu")(x)
-    x = Dropout(0.5)(x)
-    if classes <=1:
-      outputs = layers.Dense(classes, activation="sigmoid")(x)
-    else:
-      outputs = layers.Dense(classes, activation="softmax")(x)
-  
-    model = keras.Model(inputs=inputs, outputs=outputs)
-    return model
+    @staticmethod
+    def timeseries_transformer(input_length,
+                               embed_dim=30, num_heads=2, ff_dim=2,
+                               classes=1, dense_units=20):
+        """
+        TimeSeries Transformer Classifier Instance.
+        :param embed_dim: Embedding size for each token.
+        :param num_heads: Number of attention heads.
+        :param ff_dim: Hidden layer size in feed forward network inside transformer.
+        """
 
-  def get_model(self) -> Model:
-    transformer = self.TimeSeriesTransformer(input_length=500,
-                                            embed_dim=30, num_heads=5, ff_dim=32,
-                                            classes=1, dense_units=32)
+        inputs = layers.Input(shape=(500, 30))
+        embedding_layer = PositionEmbedding(vocab_size=12158,
+                                            embed_dim=30,
+                                            maxlen=500)
+        x = embedding_layer(inputs)
+        transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim, )
+        x = transformer_block(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(dense_units, activation="relu")(x)
+        x = Dropout(0.5)(x)
+        if classes <= 1:
+            outputs = layers.Dense(classes, activation="sigmoid")(x)
+        else:
+            outputs = layers.Dense(classes, activation="softmax")(x)
 
-    inputs = tf.keras.layers.Input(shape=(500, 30))
-    x = transformer(inputs)
+        model = keras.Model(inputs=inputs, outputs=outputs)
+        return model
 
-    model = tf.keras.models.Model(inputs=inputs,
-                                  outputs=x, name='transformer')
-    model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
-    return model
+    def get_model(self) -> Model:
+        transformer = self.timeseries_transformer(input_length=500,
+                                                  embed_dim=30, num_heads=5, ff_dim=32,
+                                                  classes=1, dense_units=32)
+
+        inputs = tf.keras.layers.Input(shape=(500, 30))
+        x = transformer(inputs)
+
+        model = tf.keras.models.Model(inputs=inputs,
+                                      outputs=x, name='transformer')
+        model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+        return model
