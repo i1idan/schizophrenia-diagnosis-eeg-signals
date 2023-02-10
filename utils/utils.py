33db978b4c64801324a43f60816dbc51e3700f0d
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-02-11 00:10:23 +0330

    fix windows issue in utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 def save_params(param_path, args):
     print("[INFO] Saving params!")
     with open(param_path, mode='w') as f:
-        arguments = vars(args)
+        arguments = args if isinstance(args, dict) else vars(args)
         for key, val in arguments.items():
             f.write(f"{key} {val}\n")
     print("[INFO] Params are successfully saved!")
