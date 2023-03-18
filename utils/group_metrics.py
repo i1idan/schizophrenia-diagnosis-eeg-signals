Fix metric calculation process

utils/group_metrics.py
@@ -5,15 +5,18 @@ import numpy as np
 
 def get_mean_std(csv_lists,
                  arguments=("accuracy", "loss", "val_accuracy", "val_loss"),
-                 operators=(max, min, max, min)
+                 # operator=(max, min, max, min),
+                 get_min_index_op=np.argmin,
+                 get_min_index_metric="val_loss",
                  ):
     metrics = defaultdict(list)
     print(f"[INFO] Extracting values from csv files...")
     for csv in csv_lists:
         print(f"[INFO] Getting the values of file {csv}")
-        csv_file = pd.read_csv(csv)
-        for metric, op in zip(arguments, operators):
-            val = op(csv_file[metric])
+        df = pd.read_csv(csv)
+        index = get_min_index_op(df[get_min_index_metric])
+        for metric in arguments:
+            val = df[metric][index]
             metrics[metric].append(val)
 
     metrics = {metric: {"std": round(np.std(val_list), 4), "mean": round(np.mean(val_list), 4)} for metric, val_list in
@@ -48,10 +51,21 @@ def get_conf_mean_std(conf_matrices):
 
 
 if __name__ == '__main__':
-    metrics = get_mean_std(
-        csv_lists=["/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_37_26.379079/log.csv",
-                   "/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_09_40.034447/log.csv",
-                   "/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_07_52.880768/log.csv",
-                   "/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_06_43.842120/log.csv",
-                   ])
-    print(metrics)
+    # metrics = get_mean_std(
+    #     csv_lists=["/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_37_26.379079/log.csv",
+    #                "/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_09_40.034447/log.csv",
+    #                "/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_07_52.880768/log.csv",
+    #                "/home/ai/projects/schizo/checkpoints/FFTCustom/_2022-01-06_13_06_43.842120/log.csv",
+    #                ])
+    # print(metrics)
+    import os
+
+    multi_train = 10
+    model_names = ["WaveletCustom", "FFTCustom", "Transformer", "conv_lstm"]
+    checkpoints = "../checkpoints"
+    for model_name in model_names:
+        print(f"[INFO] Processing Model: {model_name}")
+        csv_files = [os.path.join(checkpoints, model_name, f"{n}", "log.csv") for n in range(multi_train)]
+        metrics = get_mean_std(csv_files,
+                               arguments=("accuracy", "loss", "val_accuracy", "val_loss"))
+        print(model_name, "\n", metrics)